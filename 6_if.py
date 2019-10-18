bd1314e8d6e8876826b53cfa028e20f55df2c548
gonzaemon111 <goki14914@icloud.com>
2019-10-18 16:07:05 +0900

    add 6_if.py

---
+++ 6_if.py
@@ -0,0 +1,15 @@
+# coding: UTF-8
+
+score = 80
+if score > 78: # 比較演算子 > を使っています。
+    print("合格です。おめでとう！")
+else:
+    print("不合格。次回頑張りましょう。")
+
+score = 100
+if score == 100:
+    print("満点")
+elif score > 85:
+    print("合格！")
+else:
+    print("不合格。次回頑張りましょう。")
